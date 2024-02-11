3cfc53fe351bd7ac45d966c67a63bcb6d2ded4e1
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-02-11 19:52:19 +0000

    config

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+
+// "use strict"; //Treat all js code as newer version
+
+//alert(3+3) We are using nodeJs, not browser
+
+//code readibility must be first prefernece
+
+
+let name = "Tushankar"  //This will take as string data type automitically
+let id = 17;  //This will take as int data type automitically
+let isLoggedIn = false   //This will take as boolean data type automitically
+
+console.log(typeof "Tushankar")
+console.log(typeof id)
