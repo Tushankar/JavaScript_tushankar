a6fe0ecca6ae446355678b0f2d5b35ec23df5f91
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-02-13 18:52:54 +0000

    Memory

---
+++ 01_basics/Memory.js
@@ -0,0 +1,12 @@
+//****************************************************MEMORY********************************************* */
+
+
+//There are 2 Types of memory used in JavaScript
+
+//Stack Memory and Heap Memory
+
+//Primitive Types data used in stack memory
+
+//Non primitive Types data used in Heap Memory
+
+let myYoutubeName = "TushankarSaha.com"
