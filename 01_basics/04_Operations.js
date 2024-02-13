16382316864f2d4fcb7fdd315077786ae09c2e74
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-02-13 18:14:53 +0000

    Operations

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,15 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+let str1 = "Tushankar"
+let str2 = " Saha"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);//here it will take all the objects as strings as the first element is string
+console.log(1 + 4 + "2" );
