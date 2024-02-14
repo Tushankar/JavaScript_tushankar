40d13fae48fca03f2c53733b6578b91f5f736a65
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-02-14 21:59:10 +0000

    This

---
+++ 03_basics/thisArrow.js
@@ -0,0 +1,38 @@
+const user = {
+    username : "Tushankar",
+    price: 999,
+
+    WelcomeMessage: function() {
+        //this create current context 
+        console.log(`${this.username} , Welcome to website`);
+    }
+}
+
+user.WelcomeMessage()
+//if we change the username here
+//it will get changed as we used this keyword jo bhi current context hai us context ke andar jo username ke value he woh print kardo here we change the context
+user.username = "sam";
+user.WelcomeMessage()
+
+
+function chai(){
+    let userName = "Hitesh"
+    //We cant use this inside function
+    console.log(this);
+}
+
+chai()
+
+
+//Arrow function
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+//implicit return
+
+const addTwo = (num1, num2) => num1+num2
+
+console.log(addTwo(2,4));
