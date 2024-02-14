6c9f6aac58dca806bb6e4ff4697b87cd4afdaf15
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-02-14 19:35:04 +0000

    Api

---
+++ 02_basics/05_Objects.js
@@ -0,0 +1,31 @@
+//Destructuring and JSON API
+
+const course = {
+    courseName : " js in hindi ",
+    price : "3000",
+    CourseInstructor: " Hitesh "
+}
+
+//course.CourseInstructor
+//You can also give name according to yoou
+const {CourseInstructor : instructor} = course
+// console.log(CourseInstructor);
+console.log(instructor);
+
+
+// *****************************API**********************************
+
+// {
+//     //This is json not object
+//     "name": "Tushankar",
+//     "coursename": "js in hindi"
+// }
+//you can also get array as apis
+[
+
+    {},
+    {},
+    {}
+
+]
+
