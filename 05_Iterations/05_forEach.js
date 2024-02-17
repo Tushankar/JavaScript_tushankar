5f44f11921fe49e34360d67a0250b8e2bbf33442
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-02-17 22:02:42 +0000

    foreach

---
+++ 05_Iterations/05_forEach.js
@@ -0,0 +1,18 @@
+
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach(function (val){
+//     console.log(val);
+// })
+
+// coding.forEach( (val)=> {
+//     console.log(val)
+// })
+
+function printMe(item){
+    console.log(item);
+}
+
+coding.forEach(printMe)
+
