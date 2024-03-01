0c505a22d4c92fa6313240fd5264bc3a6a5aa9c9
Tushankar saha <146471775+Tushankar@users.noreply.github.com>
2024-03-01 16:52:45 +0000

    pro

---
+++ 09_Async/01_basics.js
@@ -0,0 +1,14 @@
+
+//in general javascript is syncronized and it is single threaded langauge
+//As we know from this it will work one function execution after another
+
+//Thread
+//Execute one line of code at a time (one by one)
+//jab tak purana statement excute nahi hua wo naya nahi pakadta
+//Each execution waits for the last one to complete before execution
+
+
+//(SYNCRONIZED)Blocking code(Block the flow of program) and Non Blocking code(Do not block execution)(ASYNCRONIZED)
+
+
+
